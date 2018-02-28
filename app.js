0190d3b318f90f3641491881bda12bbf696492e9
Davidchhing <davidhchhing@gmail.com>
2018-02-27 19:58:36 -0800

    added pictures to an array

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var pictures = [
+    "images/bag.jpg";
+    "images/banana.jpg";
+    "images/boots.jpg";
+    "images/chair.jpg";
+    "images/cthulhu.jpg";
+    "images/dragon.jpg";
+    "images/pen.jpg";
+    "images/scissors.jpg"
+    "images/shark.jpg";
+    "images/babysweep.jpg";
+    "images/unicorn.jpg";
+    "images/usb.jpg";
+    "images/water_can.jpg";
+    "images/wine_glass.jpg"
+]
