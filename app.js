d38c6b0f6e898034295187ecf0d0312a820d5c70
Danny Danger <dannywithachance@gmail.com>
2018-03-04 16:15:56 -0800

    added a click counter

--- app.js
+++ app.js
@@ -2,6 +2,37 @@
 
 // window.onload = choosePic;
 
+var counter = 0;
+
+function addCounter() {
+    document.getElementById("getRandomPicture").addEventListener("click", trackPicClick)
+}
+
+function trackPicClick(event) {
+    if (event.target.tagName == "IMG") {
+        var index = event.target.src.lastIndexOf("/");
+        var filePath = event.target.src.substring(index + 1);
+        for (var imageIndex = 0; imageIndex < pictureArray.length; imageIndex++); {
+            var item = pictureArray[imageIndex];
+            if (item.filePath.indexOf(filePath) !== -1) {
+                item.y++;
+            }
+        }
+        counter++;
+        displayRandomPictures();
+        if (counter == 15) {
+            alert("HOLD IT BUCKO");
+        }
+    }
+}
+
+var Product = function(name, filePath) {
+    this.label = name;
+    this.filePath = filePath;
+    this.y = 0;
+}
+
+
 var pictureArray = [
     "Pictures/bag.jpg",
     "Pictures/banana.jpg",
@@ -42,41 +73,28 @@ function displayRandomPictures() {
 
 window.addEventListener("load", displayRandomPictures);
 
- 
-// var allPictures = 
+window.addEventListener("load", function() {
+    document.getElementById("show-chart-button").addEventListener("click", showChart);
+})
 
-// function getRandomPicture(randomPic, diplayImages) {
-//     var randomPic = pictureArray[Math.floor(Math.random() * pictureArray.length)];
-//     var diplayImages = document.getElementById("getRandomPicture").innerHTML = "<img src='" + randomPic + "'>";
-//     for (var pictureIndex = 0; pictureIndex < pictureArray.length; pictureArray++)
-   
-// }
-
-// window.addEventListener("load", function() {
-//     document.getElementById("show-chart-button").addEventListener("click", showChart);
-
-
-
-// })
-
-// window.onload = function () {
-// 	var chart = new CanvasJS.Chart("chartContainer", {
-// 		title:{
-// 			text: "Bus Mall"              
-// 		},
-// 		data: [              
-// 		{
-// 			// Change type to "doughnut", "line", "splineArea", etc.
-// 			type: "column",
-// 			dataPoints: pictureArray
-// 				{ label: "apple",  y: 10  },
-// 				{ label: "orange", y: 15  },
-// 				{ label: "banana", y: 25  },
-// 				{ label: "mango",  y: 30  },
-// 				{ label: "grape",  y: 28  }
-// 			]
-// 		}
-// 		]
-// 	});
-// 	chart.render();
-// }
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "Bus Mall"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: pictureArray
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+	});
+	chart.render();
+}
